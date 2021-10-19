b6d3db67d1e5e221d7425c24853d6f2a399f32bb
juanjo5516 <juanjosedelaguilalopez@gmail.com>
2021-10-19 15:20:50 -0600

    Iniciando proveedores

--- resources/views/catalogos/proveedores.blade.php
+++ resources/views/catalogos/proveedores.blade.php
@@ -5,7 +5,4 @@
         <h1 class="text-3xl text-gray-700 mb-2 uppercase">Catálogo Proveedores</h1>
     </div>
 
-    <x-insumos-table :items="$insumos" :titles="['Código Sicoin','Nombre','Descripción','Categoría','Subcategoría','Cantidad','']" :acciones="['Eliminar']" parametroRuta="id" ruta="insumos.destroy" />
-
-
 @endsection
